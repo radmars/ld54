38187a486b55e64b63ee174da88dd8ed46a91a45
Eugene Marcotte <emarcotte@gmail.com>
2023-09-30 13:55:37 -0400

    add bevy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,76 @@
-fn main() {
+//! Entrypoint for the game jam
+// Turn on some more aggressive warnings from clippy. They shouldn't break the
+// build, but should tell you if you're doing something crazy.
+#![warn(clippy::pedantic)]
+// I hate broken links.
+#![deny(rustdoc::broken_intra_doc_links)]
+// Bevy passes queries and things by default as values which is a bit hard to
+// work around.
+#![allow(clippy::needless_pass_by_value)]
+// If it turns out we're killing precision we can open these up but they're off
+// by default so probably not a big deal
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_wrap)]
+// Sadly some systems have super complex type signatures and I'm not sure how to refactor it right now?
+#![allow(clippy::type_complexity)]
+// Turn on some stuff that isn't in pedantic.
+#![warn(
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused,
+    missing_docs,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unused_qualifications,
+    variant_size_differences
+)]
+// I'm not sure i like this 2018 idiom. Can debate it later.
+#![allow(elided_lifetimes_in_paths)]
 
-    println!("Hello, world!");
+use bevy::{prelude::*, window::WindowResolution};
+
+fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
+    let mut app = App::default();
+
+    app.add_plugins((
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    canvas: Some("#bevy".to_owned()),
+                    fit_canvas_to_parent: true,
+                    focused: true,
+                    mode: bevy::window::WindowMode::Windowed,
+                    resizable: false,
+                    resolution: WindowResolution::new(800.0, 600.0),
+                    transparent: true,
+                    ..Default::default()
+                }),
+                ..default()
+            })
+            // Fix sprite blur
+            .set(ImagePlugin::default_nearest()),
+        //   loading_plugin,
+        //  InputManagerPlugin::<Action>::default(),
+        //  PhysicsPlugins::default(),
+    ))
+    .insert_resource(Msaa::Off)
+    .insert_resource(ClearColor(Color::hex("#000000").unwrap()))
+    .add_systems(Update, bevy::window::close_on_esc)
+    .add_systems(Update, something)
+    .run();
+}
+
+fn something(k: Res<Input<KeyCode>>) {
+    if k.just_pressed(KeyCode::J) {
+        info!("Hohoho");
+    }
 }
