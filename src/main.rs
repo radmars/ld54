5f71222dd9d01c565c749a314b12111de0a5ad71
Adhesion <adhesionmusic@gmail.com>
2023-10-01 23:21:27 -0400

    limited space HAHAHAHAH!!!! AHHH!!!

--- src/main.rs
+++ src/main.rs
@@ -999,7 +999,6 @@ fn player_input_map() -> InputMap<Action> {
         }),
         Action::Move,
     );
-    input_map.insert(KeyCode::Space, Action::Jump);
     input_map.insert(KeyCode::Up, Action::Jump);
     input_map.insert(GamepadButtonType::South, Action::Jump);
     input_map
